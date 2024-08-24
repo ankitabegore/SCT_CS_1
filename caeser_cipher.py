a4c1c780913bea50d7ece4123db79a86299e61b1
ankitabegore <157394463+ankitabegore@users.noreply.github.com>
2024-08-24 14:24:57 +0530

    Update caeser_cipher.py

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,4 +1,4 @@
-def caesar_cipher(message, shift):
+def caesar_cipher_encrypt(message, shift):
     encrypted_message = ""
     
     for char in message:
@@ -17,9 +17,30 @@ def caesar_cipher(message, shift):
     
     return encrypted_message
 
-message = input("Enter the message to be encrypted: ")
+def caesar_cipher_decrypt(message, shift):
+    decrypted_message = ""
+    
+    for char in message:
+        if char.isupper():
+            position = ord(char) - ord('A')
+            new_position = (position - shift) % 26
+            new_char = chr(new_position + ord('A'))
+            decrypted_message += new_char
+        elif char.islower():
+            position = ord(char) - ord('a')
+            new_position = (position - shift) % 26
+            new_char = chr(new_position + ord('a'))
+            decrypted_message += new_char
+        else:
+            decrypted_message += char
+    
+    return decrypted_message
+
+message = input("Enter the message: ")
 shift = int(input("Enter the shift value: "))
 
-encrypted_message = caesar_cipher(message, shift)
-
+encrypted_message = caesar_cipher_encrypt(message, shift)
 print("Encrypted message:", encrypted_message)
+
+decrypted_message = caesar_cipher_decrypt(encrypted_message, shift)
+print("Decrypted message:", decrypted_message)
